3e5a2601abd75ed4ba0875df706a624a3c83b75b
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-04-16 23:29:46 +0300

    price type fix

--- pages/lot.php
+++ pages/lot.php
@@ -87,7 +87,7 @@
             <div class="lot-item__cost-state">
               <div class="lot-item__rate">
                 <span class="lot-item__amount">Текущая цена</span>
-                <span class="lot-item__cost"><?= preparePrice('10999') ?></span>
+                <span class="lot-item__cost"><?= preparePrice(10999) ?></span>
               </div>
               <div class="lot-item__min-cost">
                 Мин. ставка <span>12 000 р</span>
